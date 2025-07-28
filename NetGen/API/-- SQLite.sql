16499a8a22f258f1890cb46e38fb1bbb531cd2ab
spandan patel <spandan.patel@gaf.com>
2025-07-28 09:53:12 -0400

    commits

---
+++ NetGen/API/-- SQLite.sql
@@ -0,0 +1,11 @@
+-- SQLite
+CREATE TABLE Users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                email TEXT NOT NULL,
+                password TEXT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+            
+INSERT INTO Users ( email, password) VALUES ("spatel@gmail.com", "spatel123")
+INSERT INTO Users ( email, password) VALUES ("jsmith@gmail.com", "jsmith123")                
+INSERT INTO Users ( email, password) VALUES ("jdoe@gmail.com", "jdoe123")
